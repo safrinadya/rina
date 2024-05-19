3f3cdf79992b8c9b61c7c25940ccce84fc73fd47
safrina nadhya <safrinanadya90@gmail.com>
2024-05-19 10:45:26 +0700

    mengembalikaj kode di routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/product', function () {
     return view('list_product');
 });
